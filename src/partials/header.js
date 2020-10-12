bcabd39be27ccf5e4c96a8735f4d41f1ee859fce
Alex Lockhart <alexlockahrt7@gmail.com>
2020-10-12 06:58:08 -0400

    Updated header

--- src/partials/header.js
+++ src/partials/header.js
@@ -2,6 +2,7 @@ import { html } from 'orison';
 
 export default () => html`
   <header>
-    <h1>JTL <small>JSON Template Language</small></h1>
+    <h1>JTL</h1>
+    <h3>JSON Templating Language</h3>
   </header>
 `;
